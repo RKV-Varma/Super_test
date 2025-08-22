40db71e19fea2033a974336099380baf3f38f1f6
Ravi Kiran Venkata Sai Varma Gedela <gedela.ravi28@gmail.com>
2025-08-22 17:13:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-    import os
+import os
 
-    api_key = os.environ.get('MY_API_KEY')
-    if api_key:
-        print(f"API Key: {api_key}")
-    else:
-        print("API Key not found in environment variables.")
+api_key = os.environ.get('MY_API_KEY')
+if api_key:
+    print(f"API Key: {api_key}")
+else:
+    print("API Key not found in environment variables.")
+
+print('Hello')
